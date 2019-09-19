8fc654ddbbb8cb1f04dfa9360168e93466da0385
Carsten König <Carsten.Koenig@hotmail.de>
2019-09-19 07:38:11 +0200

    Users Model und Datenbank

---
+++ Backend/Haskell/src/Db/Users.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, RecordWildCards #-}
+
+module Db.Users
+  ( User (..)
+  , createTables
+  , getUser
+  , insertUser
+  , deleteUser
+  , updateUser
+  ) where
+
+import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Reader (MonadReader)
+import           Data.Maybe (listToMaybe)
+import           Database.SQLite.Simple (NamedParam(..))
+import qualified Database.SQLite.Simple as Sql
+import           Db.Internal
+import           Models.User
+
+
+createTables :: (MonadReader Handle m, MonadIO m) => m ()
+createTables = useHandle $ \conn ->
+  Sql.execute_ conn
+    "CREATE TABLE IF NOT EXISTS users (\
+    \name NVARCHAR(255) PRIMARY KEY, \
+    \pwHash NVARCHAR(255) NOT NULL) WITHOUT ROWID"
+
+getUser :: (MonadReader Handle m, MonadIO m) => UserName -> m (Maybe User)
+getUser userName = useHandle $ \conn ->
+  listToMaybe . map toUser <$> Sql.query conn "SELECT name, pwHash FROM users WHERE name = ?" (Sql.Only userName)
+  where
+    toUser (name, hash) = User name hash
+
+
+insertUser :: (MonadReader Handle m, MonadIO m) => User -> m ()
+insertUser User{..} = useHandle $ \conn ->
+  Sql.executeNamed conn "INSERT INTO users (name, pwHash) VALUES (:name,:hash)" [ ":name" := userName, ":hash" := userPwHash ]
+
+
+deleteUser :: (MonadReader Handle m, MonadIO m) => UserName -> m ()
+deleteUser userName = useHandle $ \conn ->
+  Sql.execute conn "DELETE FROM users WHERE name=?" (Sql.Only userName)
+
+
+updateUser :: (MonadReader Handle m, MonadIO m) => UserName -> User -> m ()
+updateUser oldName User{..} = useHandle $ \conn ->
+  Sql.executeNamed conn "UPDATE user SET name = :name, pwHash = :hash WHERE user = :oldName" [":oldName" := oldName, ":name" := userName, ":hash" := userPwHash]
---
+++ Backend/Haskell/src/Models/User.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TemplateHaskell, RecordWildCards, NamedFieldPuns #-}
+module Models.User
+  ( Login (..)
+  , User (..)
+  , UserName
+  , create
+  , validate
+  ) where
+
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Crypto.BCrypt as BC
+import           Data.Aeson.TH (deriveJSON, defaultOptions)
+import           Data.ByteString (ByteString)
+import           Data.Text (Text)
+import           Data.Text.Encoding (encodeUtf8)
+
+type UserName = Text
+
+data Login = Login 
+  { name     :: UserName
+  , password :: Text
+  } deriving Show
+
+$(deriveJSON defaultOptions ''Login)
+
+data User = User 
+  { userName   :: UserName
+  , userPwHash :: ByteString
+  } deriving Show
+
+
+create :: MonadIO m => Login -> m (Maybe User)
+create Login{..} =
+  fmap (User name) <$> pwHash
+  where
+  pwHash = do
+    let encoded = encodeUtf8 password
+    liftIO $ BC.hashPasswordUsingPolicy BC.fastBcryptHashingPolicy encoded
+
+validate :: User -> Login -> Bool
+validate User{userPwHash} Login{password} =
+  BC.validatePassword userPwHash $ encodeUtf8 password
