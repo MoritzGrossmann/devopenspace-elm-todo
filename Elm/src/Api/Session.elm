0e6256f4a0aaa1f6e662c65b3ce1a5244237e9aa
moritz.grossmann95@googlemail.com <Troopers95?>
2019-09-19 22:21:34 +0200

    login mit basic auth

--- Elm/src/Api/Session.elm
+++ Elm/src/Api/Session.elm
@@ -1,21 +1,34 @@
 module Api.Session exposing (post)
 
+import Base64
 import Http
 import Json.Encode as Enc
 import Session exposing (Session)
+import String.Interpolate as String
 
 
 post : Session -> (Result Http.Error String -> msg) -> String -> String -> Cmd msg
 post session toMsg username password =
-    let
-        json =
-            Enc.object
-                [ ( "username", Enc.string username )
-                , ( "password", Enc.string password )
-                ]
-    in
-    Http.post
-        { url = Session.makeApiUrl session [ "login" ] [] Nothing
-        , body = Http.jsonBody json
+    Http.request
+        { method = "GET"
+        , headers =
+            [ basicAuthHeader username password
+            , Http.header "Content-Type" "application/json"
+            , Http.header "Access-Control-Request-Method" "GET"
+            , Http.header "Origin" "127.0.0.1"
+            ]
+        , url = Session.makeApiUrl session [ "user", "login" ] [] Nothing
+        , body = Http.emptyBody
         , expect = Http.expectString toMsg
+        , timeout = Nothing
+        , tracker = Nothing
         }
+
+
+basicAuthHeader : String -> String -> Http.Header
+basicAuthHeader username password =
+    let
+        up =
+            String.interpolate "{0}:{1}" [ username, password ]
+    in
+    Http.header "Authorization" (String.interpolate "Basic {0}" [ Base64.encode up ])
