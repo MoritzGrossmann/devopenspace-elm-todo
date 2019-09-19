de106bef965e5a4f15c9bb90bfa36dc764cf68ea
Carsten König <Carsten.Koenig@hotmail.de>
2019-09-19 08:49:11 +0200

    auf Stackage LTS-14.6 und Ws entfernt

--- Backend/Haskell/src/Api/TodosApi.hs
+++ Backend/Haskell/src/Api/TodosApi.hs
@@ -16,8 +16,6 @@ import           Data.Text (Text)
 import qualified Db
 import qualified Db.Tasks as Db
 import           Servant
-import           Servant.API.WebSocket (WebSocket)
-import qualified Ws
 
 
 type TodosApi =
@@ -27,15 +25,14 @@ type TodosApi =
     :<|> ReqBody '[JSON] Text :> Post '[JSON] Db.Task
     :<|> Capture "id" Db.TaskId :> Delete '[JSON] [Db.Task]
     :<|> Capture "id" Db.TaskId :> Get '[JSON] Db.Task
-    :<|> "listen" :> WebSocket
   )
 
 
-server :: Db.Handle -> Ws.Handle -> Server TodosApi
-server dbHandle wsHandle = hoistServer (Proxy :: Proxy TodosApi) toHandle todoHandlers
+server :: Db.Handle -> Server TodosApi
+server dbHandle = hoistServer (Proxy :: Proxy TodosApi) toHandle todoHandlers
   where
     todoHandlers =
-      getAllHandler :<|> updateHandler :<|> newHandler :<|> deleteHandler :<|> queryHandler :<|> wsHandler
+      getAllHandler :<|> updateHandler :<|> newHandler :<|> deleteHandler :<|> queryHandler
 
     getAllHandler =
       Db.listTasks
@@ -43,7 +40,6 @@ server dbHandle wsHandle = hoistServer (Proxy :: Proxy TodosApi) toHandle todoHa
     updateHandler task = do
       liftIO $ putStrLn $ "updating task " ++ show (Db.id task)
       Db.modifyTask task
-      Ws.broadcast wsHandle (Ws.UpdateTask task)
       found <- Db.getTask (Db.id task)
       case found of
         Nothing -> throwError notFound
@@ -52,13 +48,11 @@ server dbHandle wsHandle = hoistServer (Proxy :: Proxy TodosApi) toHandle todoHa
     newHandler txt = do
       task <- Db.insertTask txt
       liftIO $ putStrLn $ "created new task - redirecting to " ++ show (Db.id task)
-      Ws.broadcast wsHandle (Ws.NewTask task)
       return task
 
     deleteHandler tId = do
       Db.deleteTask tId
       liftIO $ putStrLn $ "deleted task " ++ show tId
-      Ws.broadcast wsHandle (Ws.DeleteTask tId)
       Db.listTasks
 
     queryHandler tId = do
@@ -68,12 +62,9 @@ server dbHandle wsHandle = hoistServer (Proxy :: Proxy TodosApi) toHandle todoHa
         Nothing -> throwError notFound
         Just task -> return task
 
-    wsHandler =
-      Ws.connect wsHandle
-
     toHandle :: ReaderT Db.Handle Handler a -> Handler a
     toHandle r = runReaderT r dbHandle
 
 
-notFound :: ServantErr
+notFound :: ServerError
 notFound = err404 { errBody = "sorry - don't know this task" }
