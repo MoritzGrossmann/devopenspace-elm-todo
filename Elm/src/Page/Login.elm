52499b7d9987a9f56b421089562a2da4b4dc86de
Carsten König <Carsten.Koenig@hotmail.de>
2019-10-07 14:22:58 +0200

    Register auf Login-Page

--- Elm/src/Page/Login.elm
+++ Elm/src/Page/Login.elm
@@ -23,12 +23,59 @@ import Session exposing (Session)
 
 
 type alias Model mainMsg =
+    { session : Session
+    , map : Msg -> mainMsg
+    , transitionTo : Maybe Route
+    , modus : Modus
+    , result : WebData ()
+    }
+
+
+type Modus
+    = Login LoginModel
+    | Register RegisterModel
+
+
+type alias LoginModel =
     { username : String
     , password : String
-    , session : Session
-    , login : WebData ()
-    , transitionTo : Maybe Route
-    , map : Msg -> mainMsg
+    }
+
+
+type alias RegisterModel =
+    { username : String
+    , password : String
+    , repeatPassword : String
+    }
+
+
+isLoading : Model mainMsg -> Bool
+isLoading model =
+    RemoteData.isLoading model.result
+
+
+isValidInput : Model mainMsg -> Bool
+isValidInput model =
+    case model.modus of
+        Login loginModel ->
+            not (String.isEmpty <| String.trim loginModel.username) && not (String.isEmpty <| String.trim loginModel.password)
+
+        Register registerModel ->
+            not (String.isEmpty <| String.trim registerModel.username) && not (String.isEmpty <| String.trim registerModel.password) && registerModel.password == registerModel.repeatPassword
+
+
+emptyLogin : LoginModel
+emptyLogin =
+    { username = ""
+    , password = ""
+    }
+
+
+emptyRegister : RegisterModel
+emptyRegister =
+    { username = ""
+    , password = ""
+    , repeatPassword = ""
     }
 
 
@@ -41,8 +88,11 @@ type Msg
     = NoOp
     | UpdateUsername String
     | UpdatePassword String
+    | UpdateRepeatPassword String
     | Submit
-    | LoginResult (Result Http.Error (Session -> Session))
+    | RemoteResult (Result Http.Error (Session -> Session))
+    | SwitchToRegister
+    | SwitchToLogin
 
 
 init : (Msg -> mainMsg) -> Session -> Maybe Route -> ( Model mainMsg, Cmd msg )
@@ -51,12 +101,11 @@ init wrapMsg session transitionTo =
         resetedSession =
             Auth.clearAuthentication session
     in
-    ( { username = ""
-      , password = ""
-      , session = resetedSession
-      , login = RemoteData.NotAsked
-      , transitionTo = transitionTo
+    ( { session = resetedSession
       , map = wrapMsg
+      , transitionTo = transitionTo
+      , modus = Login emptyLogin
+      , result = RemoteData.NotAsked
       }
     , Auth.updateLocalStorage resetedSession.authentication
     )
@@ -64,6 +113,76 @@ init wrapMsg session transitionTo =
 
 update : Msg -> Model mainMsg -> ( Model mainMsg, Cmd mainMsg )
 update msg model =
+    let
+        ( changedMain, mainCmd ) =
+            updateMain msg model
+    in
+    case changedMain.modus of
+        Login loginModel ->
+            let
+                ( newLoginModel, loginCmd ) =
+                    updateLogin changedMain msg loginModel
+            in
+            ( { changedMain | modus = Login newLoginModel }, Cmd.batch [ mainCmd, loginCmd ] )
+
+        Register registerModel ->
+            let
+                ( newRegisterModel, registerCmd ) =
+                    updateRegister changedMain msg registerModel
+            in
+            ( { changedMain | modus = Register newRegisterModel }, Cmd.batch [ mainCmd, registerCmd ] )
+
+
+updateMain : Msg -> Model mainMsg -> ( Model mainMsg, Cmd mainMsg )
+updateMain msg model =
+    case msg of
+        SwitchToLogin ->
+            ( { model
+                | result = RemoteData.NotAsked
+                , modus = Login emptyLogin
+              }
+            , Cmd.none
+            )
+
+        SwitchToRegister ->
+            ( { model
+                | result = RemoteData.NotAsked
+                , modus = Register emptyRegister
+              }
+            , Cmd.none
+            )
+
+        RemoteResult res ->
+            case res of
+                Ok updateSession ->
+                    let
+                        newSession =
+                            updateSession model.session
+                    in
+                    ( { model | session = newSession, result = RemoteData.succeed () }
+                    , Cmd.batch
+                        [ Routes.navigateTo model.session (model.transitionTo |> Maybe.withDefault Routes.Lists)
+                        , Auth.updateLocalStorage newSession.authentication
+                        ]
+                        |> Cmd.map model.map
+                    )
+
+                Err error ->
+                    ( { model | result = RemoteData.Failure error }, Cmd.none )
+
+        Submit ->
+            if isValidInput model then
+                ( { model | result = RemoteData.Loading }, Cmd.none )
+
+            else
+                ( model, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+updateLogin : Model mainMsg -> Msg -> LoginModel -> ( LoginModel, Cmd mainMsg )
+updateLogin mainModel msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
@@ -74,31 +193,56 @@ update msg model =
         UpdatePassword password ->
             ( { model | password = password }, Cmd.none )
 
+        UpdateRepeatPassword _ ->
+            ( model, Cmd.none )
+
         Submit ->
-            ( { model | login = RemoteData.Loading }
-            , Cmd.map model.map (Auth.httpLogin model.session.flags LoginResult model.username model.password)
-            )
+            if isValidInput mainModel then
+                ( model, Cmd.map mainModel.map (Auth.httpLogin mainModel.session.flags RemoteResult model.username model.password) )
 
-        LoginResult res ->
-            case res of
-                Ok updateSession ->
-                    let
-                        newSession =
-                            updateSession model.session
-                    in
-                    ( { model
-                        | session = newSession
-                        , login = RemoteData.succeed ()
-                      }
-                    , Cmd.batch
-                        [ Routes.navigateTo model.session (model.transitionTo |> Maybe.withDefault Routes.Lists)
-                        , Auth.updateLocalStorage newSession.authentication
-                        ]
-                        |> Cmd.map model.map
-                    )
+            else
+                ( model, Cmd.none )
 
-                Err error ->
-                    ( { model | login = RemoteData.Failure error }, Cmd.none )
+        RemoteResult _ ->
+            ( model, Cmd.none )
+
+        SwitchToRegister ->
+            ( model, Cmd.none )
+
+        SwitchToLogin ->
+            ( model, Cmd.none )
+
+
+updateRegister : Model mainMsg -> Msg -> RegisterModel -> ( RegisterModel, Cmd mainMsg )
+updateRegister mainModel msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        UpdateUsername username ->
+            ( { model | username = username }, Cmd.none )
+
+        UpdatePassword password ->
+            ( { model | password = password }, Cmd.none )
+
+        UpdateRepeatPassword password ->
+            ( { model | repeatPassword = password }, Cmd.none )
+
+        Submit ->
+            if isValidInput mainModel then
+                ( model, Cmd.map mainModel.map (Auth.httpRegister mainModel.session.flags RemoteResult model.username model.password) )
+
+            else
+                ( model, Cmd.none )
+
+        RemoteResult _ ->
+            ( model, Cmd.none )
+
+        SwitchToRegister ->
+            ( model, Cmd.none )
+
+        SwitchToLogin ->
+            ( model, Cmd.none )
 
 
 subscriptions : Model mainMsg -> Sub mainMsg
@@ -110,27 +254,87 @@ view : Model mainMsg -> Html mainMsg
 view model =
     H.section
         [ Attr.class "todoapp" ]
+        (case model.modus of
+            Login loginModel ->
+                viewLogin model loginModel
+
+            Register registerModel ->
+                viewRegister model registerModel
+        )
+        |> H.map model.map
+
+
+viewLogin : Model mainMsg -> LoginModel -> List (Html Msg)
+viewLogin mainModel loginModel =
+    [ H.section
+        [ Attr.class "todoapp" ]
         [ H.h1 [] [ H.text "Login" ]
         , H.form [ Ev.onSubmit Submit ]
             [ H.input
                 [ Attr.class "new-todo"
-                , Attr.disabled (RemoteData.isLoading model.login)
+                , Attr.disabled (isLoading mainModel)
                 , Attr.placeholder "username"
                 , Attr.autofocus True
-                , Attr.value model.username
+                , Attr.value loginModel.username
                 , Ev.onInput UpdateUsername
                 ]
                 []
             , H.input
                 [ Attr.class "new-todo"
-                , Attr.disabled (RemoteData.isLoading model.login)
+                , Attr.disabled (isLoading mainModel)
                 , Attr.placeholder "password"
-                , Attr.value model.password
+                , Attr.value loginModel.password
                 , Attr.type_ "password"
                 , Ev.onInput UpdatePassword
                 ]
                 []
-            , H.button [ Attr.style "display" "none", Attr.type_ "submit" ] []
+            , viewSubmitButton mainModel
             ]
         ]
-        |> H.map model.map
+    , H.h2 [] [ H.button [ Attr.class "clear-completed", Ev.onClick SwitchToRegister ] [ H.text "register" ] ]
+    ]
+
+
+viewRegister : Model mainMsg -> RegisterModel -> List (Html Msg)
+viewRegister mainModel registerModel =
+    [ H.section
+        [ Attr.class "todoapp" ]
+        [ H.h1 [] [ H.text "Register" ]
+        , H.form [ Ev.onSubmit Submit ]
+            [ H.input
+                [ Attr.class "new-todo"
+                , Attr.disabled (isLoading mainModel)
+                , Attr.placeholder "username"
+                , Attr.autofocus True
+                , Attr.value registerModel.username
+                , Ev.onInput UpdateUsername
+                ]
+                []
+            , H.input
+                [ Attr.class "new-todo"
+                , Attr.disabled (isLoading mainModel)
+                , Attr.placeholder "password"
+                , Attr.value registerModel.password
+                , Attr.type_ "password"
+                , Ev.onInput UpdatePassword
+                ]
+                []
+            , H.input
+                [ Attr.class "new-todo"
+                , Attr.disabled (isLoading mainModel)
+                , Attr.placeholder "repeat password"
+                , Attr.value registerModel.repeatPassword
+                , Attr.type_ "password"
+                , Ev.onInput UpdateRepeatPassword
+                ]
+                []
+            , viewSubmitButton mainModel
+            ]
+        ]
+    , H.h2 [] [ H.button [ Attr.class "clear-completed", Ev.onClick SwitchToLogin ] [ H.text "login" ] ]
+    ]
+
+
+viewSubmitButton : Model mainMsg -> Html Msg
+viewSubmitButton mainModel =
+    H.button [ Attr.style "display" "none", Attr.type_ "submit", Attr.disabled (not <| isValidInput mainModel) ] []
