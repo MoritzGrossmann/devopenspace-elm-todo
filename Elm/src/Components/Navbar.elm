c8fcc6fb16837eec2427a1a64e61a89fdd305316
Carsten König <carsten@gettingsharper.de>
2019-10-07 20:47:05 +0200

    kleine Änderung an Navbar

--- Elm/src/Components/Navbar.elm
+++ Elm/src/Components/Navbar.elm
@@ -11,10 +11,21 @@ import Session exposing (Session)
 
 view : Session -> Html msg
 view session =
-    H.div [ Attr.class "navbar" ]
-        [ H.h2 [] [ H.text ("Hallo " ++ ((Auth.getUserName session) |> Maybe.withDefault "")) ]
-        , H.span [ Attr.class "logout-btn" ]
-            [ H.a [ Attr.href (Routes.routeToUrlString session.flags.baseUrlPath Routes.Login) ]
-                [ H.text "logout" ]
-            ]
-        ]
+    case Auth.getUserName session of
+        Just userName ->
+            H.div [ Attr.class "navbar" ]
+                [ H.h2 [] [ H.text ("Hello " ++ userName) ]
+                , H.span [ Attr.class "logout-btn" ]
+                    [ H.a [ Attr.href (Routes.routeToUrlString session.flags.baseUrlPath Routes.Login) ]
+                        [ H.text "logout" ]
+                    ]
+                ]
+
+        Nothing ->
+            H.div [ Attr.class "navbar" ]
+                [ H.h2 [] [ H.text "please " ]
+                , H.span [ Attr.class "logout-btn" ]
+                    [ H.a [ Attr.href (Routes.routeToUrlString session.flags.baseUrlPath Routes.Login) ]
+                        [ H.text "log in" ]
+                    ]
+                ]
