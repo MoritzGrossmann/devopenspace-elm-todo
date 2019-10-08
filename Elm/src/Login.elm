e6bac0ec211cbbc630393152cdfbeb107fe1a083
Carsten König <Carsten.Koenig@hotmail.de>
2019-10-08 11:08:08 +0200

    Step2 vorbereitet

--- Elm/src/Login.elm
+++ Elm/src/Login.elm
@@ -14,14 +14,11 @@ import Html as H exposing (Html)
 import Html.Attributes as Attr
 import Html.Events as Ev
 import Http
-import Json.Encode as Enc
 import Session exposing (Session)
 
 
 type alias Model =
     { session : Session
-    , username : String
-    , password : String
     }
 
 
@@ -32,16 +29,11 @@ type alias Model =
 
 type Msg
     = NoOp
-    | UpdateUsername String
-    | UpdatePassword String
-    | Submit
 
 
 init : Session -> ( Model, Cmd msg )
 init session =
     ( { session = session
-      , username = ""
-      , password = ""
       }
     , Cmd.none
     )
@@ -53,15 +45,6 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        UpdateUsername username ->
-            ( { model | username = username }, Cmd.none )
-
-        UpdatePassword password ->
-            ( { model | password = password }, Cmd.none )
-
-        Submit ->
-            ( model, Cmd.none )
-
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
@@ -70,31 +53,4 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    H.section
-        [ Attr.class "todoapp" ]
-        [ H.h1 [] [ H.text "Login" ]
-        , H.form [ Ev.onSubmit Submit ]
-            [ H.input
-                [ Attr.class "new-todo"
-                , Attr.placeholder "username"
-                , Attr.autofocus True
-                , Attr.value model.username
-                , Ev.onInput UpdateUsername
-                ]
-                []
-            , H.input
-                [ Attr.class "new-todo"
-                , Attr.placeholder "password"
-                , Attr.value model.password
-                , Attr.type_ "password"
-                , Ev.onInput UpdatePassword
-                ]
-                []
-            , viewSubmitButton model
-            ]
-        ]
-
-
-viewSubmitButton : Model -> Html Msg
-viewSubmitButton model =
-    H.button [ Attr.type_ "submit" ] [ H.text "OK" ]
+    H.h1 [] [ H.text "LOGIN" ]
--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 
 in diesen Schritt füllen wir das Login etwas mit Leben
 
-## Übung
-
-Der **OK** Knopf soll `disabled` bleiben solange `username` oder `password`
-leer sind.
+- App-Aufbau durchgehen (Module) und erklären
+- in `Login` ein einfaches Login-Formular erstellen
