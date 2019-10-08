5db38b7534db8c17e0ee2e07834a6dc936e4b14a
Carsten König <Carsten.Koenig@hotmail.de>
2019-10-08 07:34:47 +0200

    Backend und Docker-Konfig für Endfassung Teilnehmer entfernt

--- Elm/ports.js
+++ Elm/ports.js
@@ -1,24 +1,31 @@
 window.initPorts = function (app) {
-  app.ports.store.subscribe(function (data) {
-    let storageKey = data[0];
-    let value = data[1];
-    if (value === null) {
-      localStorage.removeItem(storageKey);
-    } else {
-      localStorage.setItem(storageKey, value);
-    }
-  });
+  if (app && app.ports) {
 
-  window.addEventListener("storage", function (e) {
-    if (app.ports.receive) {
-      app.ports.receive.send([e.key, e.newValue]);
+    if (app.ports.store) {
+      app.ports.store.subscribe(function (data) {
+        if (data && data.length >= 2) {
+          let storageKey = data[0];
+          let value = data[1];
+          if (value === null) {
+            localStorage.removeItem(storageKey);
+          } else {
+            localStorage.setItem(storageKey, value);
+          }
+        }
+      });
     }
-  });
 
-  app.ports.request.subscribe(function (storageKey) {
-    let value = localStorage.getItem(storageKey);
     if (app.ports.receive) {
-      app.ports.receive.send([storageKey, value]);
+      window.addEventListener("storage", function (e) {
+        app.ports.receive.send([e.key, e.newValue]);
+      });
     }
-  });
+
+    if (app.ports.request && app.ports.receive) {
+      app.ports.request.subscribe(function (storageKey) {
+        let value = localStorage.getItem(storageKey);
+        app.ports.receive.send([storageKey, value]);
+      });
+    }
+  }
 }
